7ba564527b04dbe15fa2ae5fe4807a3c03a79689
Natalí Rodrigues <natalyrodrigues43210@gmail.com>
2022-10-24 23:36:46 -0300

    adicionando atributos à classe Animal

--- apredendoPolimorfismo/src/apredendoPolimorfismo/Animal.java
+++ apredendoPolimorfismo/src/apredendoPolimorfismo/Animal.java
@@ -1,5 +1,12 @@
 package apredendoPolimorfismo;
 
 public class Animal {
+	// atriutos cor, peso, tamanho e espécie
+	
+	private String cor;
+	private double peso;
+	private double tamanho;
+	private String especie;
+	
 
 }
